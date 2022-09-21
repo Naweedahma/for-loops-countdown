bd68a5d70ad0de4fe74e6785f98bc6129d05f2cf
Naweedahma <100299521+Naweedahma@users.noreply.github.com>
2022-09-21 10:44:42 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 10; index++) {
+        basic.showNumber(10 - index)
+    }
+})
+basic.showIcon(IconNames.Heart)
+let index = 9
+basic.forever(function () {
+	
+})
